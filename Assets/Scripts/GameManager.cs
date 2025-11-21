95698a55c7dec0ffe24d626afe0a8d79e29e5043
DrFeelGoodz <agabryluk3@gatech.edu>
2025-11-20 21:54:14 -0500

    WebGL settings

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,10 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-
+    public int trials = 6;
+    public List<string> environments = new List<string>() {"nature", "office", "stone", "ocean", "shop"};
     void Start()
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    public void GenerateExperiment()
+    {
+        foreach(string env in environments)
+        {
+            Block exp_block = Session.instance.CreateBlock(trials);
+            for(int i = 0; i < trials; i++)
+            {
+                exp_block.GetRelativeTrial(i + 1).settings.SetValue("environment", env);
+                if(i < trials - 1)
+                    exp_block.GetRelativeTrial(i + 1).settings.SetValue("spawn", "start");
+                else
+                    exp_block.GetRelativeTrial(i + 1).settings.SetValue("spawn", "wrong");
+            }
+        }
     }
     public void LoadScene()
     {
